797b982e096f9301ee5d9e67a51374427f5eb28f
Ziratsu <enzo.ustariz@hotmail.fr>
2021-04-09 16:40:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', loader);
+
+function loader(){
+
+    const TLLOAD = gsap.timeline();
+
+    TLLOAD
+    .to('.images-container', {height: 400, duration: 1.3, delay: 0.4, ease: 'power2.out'})
+    .to('.bloc-txt', {height: 'auto', duration: 0.6, ease: 'power2.out'}, '-=0.8')
+    .to('.bloc-txt h2', {y: 0, ease: 'power2.out'}, '-=0.6')
+
+    .to('.f2', {y: 0, duration: 0.4, ease: 'power2.out'})
+    .add(() => {
+        document.querySelector('.flip-img1').style.backgroundImage = "url('ressources/image1.jpg')";
+    })
+    .to('.f2', {y: '-100%'})
+
+    .to('.load-container', {opacity: 0, duration: 0.8, delay: 0.7})
+    .add(() => {
+        document.querySelector('.load-container').style.display = "none";
+    })
+    .add(() => {
+        document.querySelector('video').play()
+    }, '-=0.8')
+
+}
